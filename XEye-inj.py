4d334d2b101f4cbb4ea8eb04e04b0e46640cc17b
Mostafa Ahmad <53053339+Engmostafa26@users.noreply.github.com>
2022-10-24 18:30:59 +0200

    Update XEye-inj.py

--- XEye-inj.py
+++ XEye-inj.py
@@ -76,3 +76,7 @@ def packinjector(packet):
 que = netfilterqueue.NetfilterQueue()
 que.bind(0, packinjector)
 que.run()
+print("\t\t\t\t[*] Thanks for using XEye-tp. Below are our Social Media OSINT Hacking bundle recommended for you :) [*]")
+print("\n [***] --> The Ultimate Social Media OSINT Hacking Bundle(70% OFF): https://rb.gy/sgxib8")
+print("*******************************************************************************************************")
+print("\n [Author] Eng.Mostafa Ahmad - Cybersecurity Expert and \"XEye\" founder.")
